713d30214963223de1448baaa1f2bae05eed1727
Christopher Baldauf <cbaldauf@vistaprint.com>
2015-11-23 15:10:03 -0500

    Also take ownership of provisioner assets, because that was failing for our real-world use case.

--- lib/vagrant-managed-servers/action/take_synced_folder_ownership.rb
+++ lib/vagrant-managed-servers/action/take_synced_folder_ownership.rb
@@ -18,15 +18,26 @@ module VagrantPlugins
           return unless machine.config.orchestrate.take_synced_folder_ownership
           ui.info "Taking ownership of all guest synced folders"
 
+          @logger.debug "Taking ownership of synced folders"
           machine.config.vm.synced_folders.each do |synced_folder|
             name = synced_folder[0]
             options = synced_folder[1]
             next if options[:disabled]
             options[:owner] ||= machine.ssh_info[:username]
-            @logger.debug "Taking ownership of #{name}"
-            @logger.debug options
-            machine.communicate.sudo "chown '#{options[:owner]}' -R #{options[:guestpath]}"
+            chown machine, options[:guestpath], options[:owner]
           end
+
+          @logger.debug "Taking ownership of provisioner assets"
+          machine.config.vm.provisioners.each do |provisioner|
+            owner = machine.ssh_info[:username]
+            chown machine, provisioner.config.upload_path, owner if provisioner.type == :shell
+            chown machine, provisioner.config.temp_dir, owner if provisioner.type == :puppet
+          end
+        end
+
+        def chown(machine, path, owner)
+          @logger.debug "Taking ownership of #{path}"
+          machine.communicate.sudo "chown '#{owner}' -R #{path}"
         end
       end
     end
--- lib/vagrant-orchestrate/version.rb
+++ lib/vagrant-orchestrate/version.rb
@@ -1,5 +1,5 @@
 module VagrantPlugins
   module Orchestrate
-    VERSION = "0.8.0.pre.2"
+    VERSION = "0.8.0.pre.3"
   end
 end
